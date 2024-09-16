98b9b267c28132661bb1689ee8af8b9ecf12a0ea
rajagopalvenkat <rajagopalvenkat@outlook,com>
2024-09-16 13:33:48 -0400

    updated MFMC skeleton

--- EAAI_EscapeTheCastle/MBMC.py
+++ EAAI_EscapeTheCastle/MBMC.py
@@ -28,34 +28,44 @@ if gui_flag:
 
 Complete the function below to do the following:
 
-	1. Run 1000 episodes of the game. An episode refers to starting in some initial configuration and taking actions until a terminal state is reached.
-	2. Keep track of gameplay history in an appropriate format for each of the 1000 episodes.
+	1. Run a specified number of episodes of the game (argument num_episodes). An episode refers to starting in some initial 
+	   configuration and taking actions until a terminal state is reached.
+	2. Keep track of gameplay history in an appropriate format for each of the episodes.
 	3. From gameplay history, estimate the probability of victory against each of the guards when taking the fight action.
 
 	Some important notes:
 
 		a. Keep in mind that given some observation [(X,Y), health, guard_in_cell], a fight action is only meaningful if the 
 		   last entry corresponding to guard_in_cell is nonzero.
+
 		b. Upon taking the fight action, if the player defeats the guard, the player is moved to a random neighboring cell with 
 		   UNCHANGED health. (2 = Full, 1 = Injured, 0 = Critical).
+
 		c. If the player loses the fight, the player is still moved to a random neighboring cell, but the health decreases by 1.
+
 		d. Your player might encounter the same guard in different cells in different episodes.
+
 		e. All interaction with the environment must be done using the env.step() method, which returns the next
 		   observation, reward, done (Bool indicating whether terminal state reached) and info. This method should be called as 
 		   obs, reward, done, info = env.step(action), where action is an integer representing the action to be taken.
+
 		f. The env.reset() method resets the environment to the initial configuration and returns the initial observation. 
 		   Do not forget to also update obs with the initial configuration returned by env.reset().
+
+		g. To simplify the representation of the state space, each state may be hashed into a unique integer value using the hash function provided above.
+		   For instance, the observation {'player_position': (1, 2), 'player_health': 2, 'guard_in_cell='G4'} 
+		   will be hashed to 1*5*3*5 + 2*3*5 + 2*5 + 4 = 119. There are 375 unique states.
 
 	Finally, return the np array, P which contains four float values, each representing the probability of defeating guards 1-4 respectively.
 
 '''
 
-def estimate_victory_probability():
+def estimate_victory_probability(num_episodes=1000):
 	"""
     Probability estimator
 
     Parameters:
-    - None
+    - num_episodes (int): Number of episodes to run.
 
     Returns:
     - P (numpy array): Empirically estimated probability of defeating guards 1-4.
---
+++ EAAI_EscapeTheCastle/MFMC.py
@@ -0,0 +1,81 @@
+import numpy as np
+from vis_gym import *
+
+gui_flag = False # Set to True to enable the game state visualization
+setup(GUI=gui_flag)
+env = game # Gym environment already initialized within vis_gym.py
+
+obs, reward, done, info = env.reset() # Reset the environment to initial configuration
+
+#env.render() # Uncomment to print game state info
+
+def hash(obs):
+	x,y = obs['player_position']
+	h = obs['player_health']
+	g = obs['guard_in_cell']
+	if not g:
+		g = 0
+	else:
+		g = int(g[-1])
+
+	return x*(5*3*5) + y*(3*5) + h*5 + g
+
+if gui_flag:
+	refresh(obs, reward, done, info)  # Update the game screen [GUI only]
+
+'''
+
+Complete the function below to do the following:
+
+	1. Run a specified number of episodes of the game (argument num_episodes). An episode refers to starting in some initial
+	   configuration and taking actions until a terminal state is reached.
+	2. Instead of saving all gameplay history, maintain and update Q-values for each state-action pair that your agent encounters in a dictionary.
+	3. Use the Q-values to select actions in an epsilon-greedy manner. Refer to assignment instructions for a refresher on this.
+	4. Update the Q-values using the Q-learning update rule. Refer to assignment instructions for a refresher on this.
+
+	Some important notes:
+		
+		- The state space is defined by the player's position (x,y), the player's health (h), and the guard in the cell (g).
+		
+		- To simplify the representation of the state space, each state may be hashed into a unique integer value using the hash function provided above.
+		  For instance, the observation {'player_position': (1, 2), 'player_health': 2, 'guard_in_cell='G4'} 
+		  will be hashed to 1*5*3*5 + 2*3*5 + 2*5 + 4 = 119. There are 375 unique states.
+
+		- Your Q-table should be a dictionary with keys as the hashed state and 
+		  values as an array of 6 floats, each representing the Q-value of one of the four actions.
+		  The autograder relies on this representation - so please don't use different representations or data structures.
+		
+		- The four actions are: 0 (UP), 1 (DOWN), 2 (LEFT), 3 (RIGHT), 4 (FIGHT), 5 (HIDE)
+
+		- Don't forget to reset the environment to the initial configuration after each episode by calling:
+		  obs, reward, done, info = env.reset()
+
+		- The value of eta is unique for every (s,a) pair, and should be updated as 1/(1 + number of updates to Q_opt(s,a)).
+
+		- The value of epsilon should be initialized to 1. You are free to choose the decay rate - experiment with different values to find what works.
+
+	Finally, return the dictionary containing the Q-values (called Q_table).
+
+'''
+
+def Q_learning(num_episodes=10000):
+	"""
+	Run Q-learning algorithm for a specified number of episodes.
+
+    Parameters:
+    - num_episodes (int): Number of episodes to run.
+
+    Returns:
+    - P (numpy array): Empirically estimated probability of defeating guards 1-4.
+    """
+	Q_table = {}
+
+	'''
+
+	YOUR CODE HERE
+
+
+	'''
+
+	return Q_table
+
