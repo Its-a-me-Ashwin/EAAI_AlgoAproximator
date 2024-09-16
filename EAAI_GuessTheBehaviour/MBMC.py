acc98d77cd7e9844df5f3a856f2b43d964caefd4
rajagopalvenkat <rajagopalvenkat@outlook,com>
2024-09-16 08:19:14 -0400

    updated instructions in MBMC

--- EAAI_GuessTheBehaviour/MBMC.py
+++ EAAI_GuessTheBehaviour/MBMC.py
@@ -31,10 +31,20 @@ Complete the function below to do the following:
 	1. Run 1000 episodes of the game. An episode refers to starting in some initial configuration and taking actions until a terminal state is reached.
 	2. Keep track of gameplay history in an appropriate format for each of the 1000 episodes.
 	3. From gameplay history, estimate the probability of victory against each of the guards when taking the fight action.
-		a. Keep in mind that given some observation [(X,Y), health, guard_in_cell], a fight action is only meaningful if the last entry corresponding to guard_in_cell is nonzero.
-		b. Upon taking the fight action, if the player defeats the guard, the player is moved to a random neighboring cell with UNCHANGED health. (2 = Full, 1 = Injured, 0 = Critical).
+
+	Some important notes:
+
+		a. Keep in mind that given some observation [(X,Y), health, guard_in_cell], a fight action is only meaningful if the 
+		   last entry corresponding to guard_in_cell is nonzero.
+		b. Upon taking the fight action, if the player defeats the guard, the player is moved to a random neighboring cell with 
+		   UNCHANGED health. (2 = Full, 1 = Injured, 0 = Critical).
 		c. If the player loses the fight, the player is still moved to a random neighboring cell, but the health decreases by 1.
 		d. Your player might encounter the same guard in different cells in different episodes.
+		e. All interaction with the environment must be done using the env.step() method, which returns the next
+		   observation, reward, done (Bool indicating whether terminal state reached) and info. This method should be called as 
+		   obs, reward, done, info = env.step(action), where action is an integer representing the action to be taken.
+		f. The env.reset() method resets the environment to the initial configuration and returns the initial observation. 
+		   Do not forget to also update obs with the initial configuration returned by env.reset().
 
 	Finally, return the np array, P which contains four float values, each representing the probability of defeating guards 1-4 respectively.
 
